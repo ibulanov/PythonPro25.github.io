5b7d35279e9932a7c2a8047afa533310f428d679
Igor Bulanov <bulanov.ig@phystech.edu>
2025-03-22 22:00:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 let tg = window.Telegram.WebApp;
+let btn = document.querySelector("#click");
 
-tg.expand()
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
 
-let btn = document.querySelector('#click')
+let item = ""
 
-btn.onclick = () => {
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
+let btn5 = document.querySelector("#btn5")
+let btn6 = document.querySelector("#btn6")
+
+btn1.onclick = () => {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide()
     } else {
-        tg.MainButton.setText("Вы выбрали котика!")
+        tg.MainButton.setText("Вы выбрали Москва-Нижний Новгород!")
+        item = "Москва-Нижний Новгород"
         tg.MainButton.show()
     }
 }
+
+btn2.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали Самара-Казань!")
+        item = "Самара-Казань"
+        tg.MainButton.show()
+    }
+}
+
+btn3.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали Ижевск-Брянск!")
+        item = "Ижевск-Брянск"
+        tg.MainButton.show()
+    }
+}
+
+btn4.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали Владивосток-Сочи!")
+        item = "Владивосток-Сочи"
+        tg.MainButton.show()
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
+})
+
+tg.expand();
